35a649fc841ab5482d332bc3d679c55f8f3da8a7
atuy1219 <yuta1219aka@gmail.com>
2025-11-29 13:15:06 +0900

    fix error

--- app/src/main/java/com/atuy/prefix_dialer/ui/theme/Theme.kt
+++ app/src/main/java/com/atuy/prefix_dialer/ui/theme/Theme.kt
@@ -41,7 +41,7 @@ fun PrefixdialerTheme(
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+        dynamicColor && true -> {
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
